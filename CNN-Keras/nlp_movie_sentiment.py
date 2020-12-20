4ad971ce78a8f0670d5c323030b265ac925ea47e
chemthan <kms.vudang@gmail.com>
2020-12-20 15:39:04 +0700

    Fixed

---
+++ CNN-Keras/nlp_movie_sentiment.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[70]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import re
+import nltk
+from nltk.corpus import stopwords
+
+from numpy import array
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten
+from keras.layers import GlobalMaxPooling1D
+from keras.layers.embeddings import Embedding
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+
+
+# In[71]:
+
+
+movie_reviews = pd.read_csv("D:\Downloads\AI\movies_datasets.csv")
+
+movie_reviews.isnull().values.any()
+
+movie_reviews.shape
+
+
+# In[72]:
+
+
+movie_reviews.head()
+
+
+# In[73]:
+
+
+movie_reviews["text"][3]
+
+
+# In[74]:
+
+
+import seaborn as sns
+
+sns.countplot(x='airline_sentiment', data=movie_reviews)
+
+
+# In[75]:
+
+
+def preprocess_text(sen):
+    # Removing html tags
+    sentence = remove_tags(sen)
+
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    return sentence
+
+
+# In[76]:
+
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub(' ',text)
+
+
+# In[77]:
+
+
+X = []
+sentences = list(movie_reviews['text'])
+for sen in sentences:
+    X.append(preprocess_text(sen))
+
+
+# In[78]:
+
+
+X[3]
+
+
+# In[79]:
+
+
+y = movie_reviews['airline_sentiment']
+
+y = np.array(list(map(lambda x: 1 if x=="positive" else 0, y)))
+
+
+# In[81]:
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+
+# In[85]:
+
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(X_train)
+
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+
+# In[86]:
+
+
+# Adding 1 because of reserved 0 index
+vocab_size = len(tokenizer.word_index) + 1
+
+maxlen = 100
+
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+
+# In[87]:
+
+
+from numpy import array
+from numpy import asarray
+from numpy import zeros
+
+embeddings_dictionary = dict()
+glove_file = open('E:/Datasets/Word Embeddings/glove.6B.100d.txt', encoding="utf8")
+
+for line in glove_file:
+    records = line.split()
+    word = records[0]
+    vector_dimensions = asarray(records[1:], dtype='float32')
+    embeddings_dictionary [word] = vector_dimensions
+glove_file.close()
+
+
+# In[88]:
+
+
+embedding_matrix = zeros((vocab_size, 100))
+for word, index in tokenizer.word_index.items():
+    embedding_vector = embeddings_dictionary.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[index] = embedding_vector
+
+
+# In[89]:
+
+
+model = Sequential()
+embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=maxlen , trainable=False)
+model.add(embedding_layer)
+
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
+
+
+# In[90]:
+
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+print(model.summary())
+
+
+# In[91]:
+
+
+history = model.fit(X_train, y_train, batch_size=128, epochs=6, verbose=1, validation_split=0.2)
+
+
+# In[92]:
+
+
+score = model.evaluate(X_test, y_test, verbose=1)
+
+
+# In[93]:
+
+
+print("Test Score:", score[0])
+print("Test Accuracy:", score[1])
+
+
+# In[96]:
+
+
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+
+# In[97]:
+
+
+model = Sequential()
+embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=maxlen , trainable=False)
+model.add(embedding_layer)
+model.add(LSTM(128))
+
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+
+# In[98]:
+
+
+print(model.summary())
+
+
+# In[99]:
+
+
+history = model.fit(X_train, y_train, batch_size=128, epochs=6, verbose=1, validation_split=0.2)
+
+score = model.evaluate(X_test, y_test, verbose=1)
+
+
+# In[100]:
+
+
+print("Test Score:", score[0])
+print("Test Accuracy:", score[1])
+
+
+# In[101]:
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+
+# In[102]:
+
+
+instance = X[57]
+print(instance)
+
+
+# In[103]:
+
+
+instance = tokenizer.texts_to_sequences(instance)
+
+flat_list = []
+for sublist in instance:
+    for item in sublist:
+        flat_list.append(item)
+
+flat_list = [flat_list]
+
+instance = pad_sequences(flat_list, padding='post', maxlen=maxlen)
+
+model.predict(instance)
+
+
+# In[ ]:
+
+
+
+
